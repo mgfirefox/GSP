be5d83564259727d938518f06d9d03f79e116858
mgfirefox <ma2ks2im8ka228@gmail.com>
2025-10-14 17:09:00 +0300

    feat: add slot indexes for shaders and buffer types for constant buffers

---
+++ ShaderUtility.h
@@ -0,0 +1,9 @@
+#pragma once
+
+constexpr uint32 VsMvpBufferSlotIndex = 0;
+
+constexpr uint32 PsMaterialBufferSlotIndex = 0;
+
+constexpr uint32 PsSamplerSlotIndex = 0;
+
+constexpr uint32 PsDiffuseColorTextureResourceSlotIndex = 0;
